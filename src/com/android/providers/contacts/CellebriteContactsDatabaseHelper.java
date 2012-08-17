498649fdbba9c6ee52adba2d8a7254a109190960
Karl-Johan Karlsson <creideiki@ferretporn.se>
2012-08-17 18:00:54 +0100

    Changed tabs to spaces.
    Change-Id: Id30a6bc5b86cf76262aa532790b6596ab67804ef

--- src/com/android/providers/contacts/CellebriteContactsDatabaseHelper.java
+++ src/com/android/providers/contacts/CellebriteContactsDatabaseHelper.java
@@ -4,24 +4,24 @@ import android.content.Context;
 import android.util.Log;
 
 public class CellebriteContactsDatabaseHelper extends ContactsDatabaseHelper {
-	private static final String TAG = "CellebriteContactsDatabaseHelper";
+    private static final String TAG = "CellebriteContactsDatabaseHelper";
 
     protected static CellebriteContactsDatabaseHelper sSingleton = null;
 
     public static synchronized ContactsDatabaseHelper getInstance(Context context) {
-    	Log.i(TAG, "getInstance");
-    	if(sSingleton == null) {
-    		Log.i(TAG, "sSingleton == null");
-    	} else {
-    		Log.i(TAG, "sSingleton is " + sSingleton.getClass().getName());
-    	}
+        Log.i(TAG, "getInstance");
+        if(sSingleton == null) {
+            Log.i(TAG, "sSingleton == null");
+        } else {
+            Log.i(TAG, "sSingleton is " + sSingleton.getClass().getName());
+        }
         if (sSingleton == null) {
             sSingleton = new CellebriteContactsDatabaseHelper(context);
         }
         return sSingleton;
     }
 
-	CellebriteContactsDatabaseHelper(Context context) {
-		super(context, "cellebrite.db");
-	}
+    CellebriteContactsDatabaseHelper(Context context) {
+        super(context, "cellebrite.db");
+    }
 }
